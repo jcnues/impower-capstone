0c371c8780d2c0ec667e7ab492b620f131b0120f
John Nuestro <john.carlo1209@gmail.com>
2025-04-07 16:44:00 -0400

    Changed cron job time to 12 UTC everyday. Added new logs, and removed logs causing issues.

--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 """
 Made by John Nuestro
 """
+
 import logging
 import json
 from datetime import timedelta, datetime
@@ -12,11 +13,13 @@ from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 10 * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=True)
+
+@app.timer_trigger(
+    schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True
+)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     """
-    Timer trigger to grab NASA APOD on a daily basis at 10am and 
+    Timer trigger to grab NASA APOD on a daily basis at 10am and
     move it to a Azure storage container
 
     Args:
@@ -28,12 +31,14 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     # initialize params
     params = {
         "date": datetime.now().strftime("%Y-%m-%d"),
-        "api_key": os.getenv("NASA_APOD_API_KEY")
+        "api_key": os.getenv("NASA_APOD_API_KEY"),
     }
     # Initialize the URL to call via api
     url = os.getenv("NASA_APOD_API_URL")
     # Run a get request and conver the repsonse to json
     apod_payload = requests.get(url=url, params=params, timeout=30).json()
+
+    logging.info("Dumping payload to temp file.")
 
     # Initialize the output filename
     output_file = "/tmp/data.json"
@@ -51,13 +56,17 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     blob_name = f"{previous_datetime.strftime('%m%d%Y')}{your_name}Data.json"
     container_name = os.getenv("CONTAINER_NAME")
 
-    logging.info(connection_string, blob_name, container_name)
+    logging.info(
+        f"Moving data from temp file into azure storage container: {container_name}"
+    )
 
     # Upload the file to the blob storage
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     container_client = blob_service_client.get_container_client(container_name)
     with open(output_file, "rb") as data:
         container_client.upload_blob(blob_name, data)
+        
+    logging.info(f"NASA APOD successfully stored into azure storage container: {container_name}")
 
     if myTimer.past_due:
         logging.info("The timer is past due!")
